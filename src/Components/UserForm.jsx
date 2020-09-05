58a2a2cfb17403f3b7e90046e4379d3b3aeb3476
kylejb <kjburda@gmail.com>
2020-09-04 23:53:32 -0400

    [DRAFT] User Component Infastructure

---
+++ src/Components/UserForm.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+const UserForm = (props) => {
+    const [email, setEmail] = useState(""),
+        [password, setPassword] = useState(""),
+        [passwordConfirmation, setPasswordConfirmation] = useState("");
+        
+        
+    const handleSubmitHelper = (e) => {
+        e.preventDefault();
+        props.handleSubmit({email: email, password: password})
+    }
+
+
+    return (
+        <>
+            <h2>UserForm (Login/Signup-Testing)</h2>
+            <form onSubmit={handleSubmitHelper}>
+                <input type="text" placeholder="email address" value={email} onChange={e => setEmail(e.target.value)}/>
+                <input type="text" placeholder="password" value={password} onChange={e => setPassword(e.target.value)}/>
+                <input type="text" placeholder="password confirmation" value={passwordConfirmation} onChange={e => setPasswordConfirmation(e.target.value)}/>
+                <input type="submit"/>
+            </form>
+        </>
+    );
+}
+
+export default UserForm;
