b5ded4888e8dc0ea097f293ab36ae8cfa544c850
kylejb <kjburda@gmail.com>
2020-09-04 23:51:02 -0400

    Experimenting with client-side routing in React (e.g., NavLink for Order History, Login/Signup to test JWT Auth, and User/Admin Dashboard component infastructure)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppRoute from './app.routes';
 import ItemsContainer from './Containers/ItemsContainer';
 import UsersContainer from './Containers/UsersContainer';
 import './App.css';
@@ -6,6 +7,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <AppRoute />
       <ItemsContainer />
       <UsersContainer />
     </div>
---
+++ src/Components/dashboard/dashboard.routes.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import Orders from './orders.component';
+import Transactions from './transactions.component';
+
+class DashboardRouter extends Component {
+
+    render() {
+        return (
+            <div>
+                <Route path="/orders" component={Orders}/>
+                <Route path="/transactions" 
+                    component={Transactions}/>
+            </div>
+        );
+    }
+}
+
+export default DashboardRouter;
