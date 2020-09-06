f3dc70c2aaac5ffa8cf66f564efe9fee5e1e8390
kylejb <kjburda@gmail.com>
2020-09-06 19:49:33 -0400

    Passed through fn 'addToCart' as prop and refactored ItemList's fn 'displayItems' to implicitly return <ItemCard />

--- src/Containers/ItemList.js
+++ src/Containers/ItemList.js
@@ -2,34 +2,20 @@ import React from 'react'
 import ItemCard from '../Components/ItemCard'
 
 
-const ItemList =(props)=>{
-    console.log("i'm inside itemlist, the following is match.url",props.url)
-    
-    const displayItems = () =>{
-       
-       return props.items.map(itemObj => (
-           
-       <ItemCard key={itemObj.id} name={itemObj.name} cost={itemObj.cost} img={itemObj.img_url} id={itemObj.id}/>
-       ))
+const ItemList = ( props ) => {
+    console.log("i'm inside itemlist, the following is match.url",  props.url)
+    console.log("i'm inside itemlist, the following is props",  props)
+
+    const displayItems = () => {
+       return props.items.map(itemObj => <ItemCard key={itemObj.id} name={itemObj.name} cost={itemObj.cost} img={itemObj.img_url} id={itemObj.id} addToCart={props.addToCart}/>)
     }
 
 
-    return(
-        <>
-        <h1> Swag to the Max</h1>
-        {displayItems()}
-
-
-        </>
-
-    )
-
-
-
-
-
-
-
+    return (
+        <div className="main">
+            {displayItems()}
+        </div>
+    );
 }
 
 export default ItemList
