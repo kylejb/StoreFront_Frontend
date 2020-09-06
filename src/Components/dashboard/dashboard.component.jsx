1b45ffde734e662254b9514cfa16a81596e6db36
kylejb <kjburda@gmail.com>
2020-09-05 20:06:51 -0400

    Minor formatting

--- src/Components/dashboard/dashboard.component.jsx
+++ src/Components/dashboard/dashboard.component.jsx
@@ -11,7 +11,7 @@ class Dashboard extends Component {
                 <nav>
                    <NavLink 
                        className="nav-main-anchor" 
-                       to="/orders">
+                       to="orders">
                        Orders
                    </NavLink>
                    <NavLink 
