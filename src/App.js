ada768e016cee8960f326b0e0862cb47eacc65fa
kylejb <kjburda@gmail.com>
2020-09-06 13:17:59 -0400

    Temporarily commented out useState hooks until Users are ready to be integrated into the Application

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AppRoute from './app.routes';
 import ItemsContainer from './Containers/ItemsContainer';
 import UsersContainer from './Containers/UsersContainer';
 import './App.css';
 
 const App = () => {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
   return (
     <div className="App">
       <AppRoute />
       <ItemsContainer />
-      <UsersContainer user={user} />
+      <UsersContainer />
     </div>
   );
 }
