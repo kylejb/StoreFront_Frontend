e649171a65c38870b46a8a00c0fddd81fb71fb7b
kylejb <kjburda@gmail.com>
2020-09-08 19:44:35 -0400

    Added unstyled order form, which is rendered onClick

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,25 @@
 import React, { Component }  from 'react';
+import PurchaseForm from './PurchaseForms/PurchaseForm'
 
 class Cart extends Component {
+
+    state = {
+        showForm: false
+    }
+
+    handleOrder = () => {
+        console.log("Cart: Clicking to go to order page")
+        return (<PurchaseForm cart={this.props.cart} />)
+    }
 
     render() {
         console.log("Cart Props ", this.props)
         return (
-            <h2>Cart</h2>
+            <>
+                <h2>Cart: checkout cart onClick...</h2>
+                <button onClick={() => this.setState({ showForm: true })} value="Place Order" />
+                { this.state.showForm ? this.handleOrder() : null }
+            </>
         );
      };
 }
