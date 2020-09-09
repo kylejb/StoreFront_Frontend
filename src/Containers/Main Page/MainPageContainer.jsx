2cb1379910a013b90e49cf11966c8b7f46342109
Luis Reyes <luisreyesxv@yahoo.com>
2020-09-09 05:57:52 -0400

    have the home page showing a slider

--- src/Containers/Main Page/MainPageContainer.jsx
+++ src/Containers/Main Page/MainPageContainer.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Slider from 'infinite-react-carousel';
+import {Link} from 'react-router-dom'
+
 
 
 class MainPageContainer extends React.Component{
@@ -9,54 +12,46 @@ class MainPageContainer extends React.Component{
 
 
     render(){
+        const settings =  {
+            arrows: false,
+            autoplay: true,
+            dots: true
+          };
         return(
-
-            <div className="parent">
-
-                    <div className="mainPage-container">
-                        <div className="image">
-                            <img src="http://lorempixel.com/702/200/" />
-                        </div>
+<div>
+        <span>CustomSlider</span>
+        <Slider { ...settings }>
+            <Link to={`/items/303/`} >
+        <div classNameName="mainPage-container">
+                         <div className="mainPageImage">
+                             <img src="http://lorempixel.com/702/200/" />
+                         </div>
                     
-                        <div className="column">
-                            <div>Name: Star Wars55</div>
-                            <div>Price: $0.08</div></div>
+                         <div classNameName="mainPage-colum">
+                             <div>Name: Star Wars55</div>
+                             <div>Price: $0.08</div></div>
                         
-                        <div>Quanity: 1</div>
+                         <div>Quanity: 1</div>
                     
-                    </div>
+                     </div>
+                     </Link>
+
+                     <div className="mainPage-container">
+                         <div className="mainPageImage">
+                             <img src="http://lorempixel.com/1002/200/" />
+                         </div>
                     
-                    <div className="mainPage-container">
-                        <div className="image">
-                            <img src="http://lorempixel.com/702/200/" />
-                        </div>
-                    
-                        <div className="column">
-                            <div>Name: Star Wars55</div>
-                            <div>Price: $0.08</div></div>
+                         <div className="mainPage-column">
+                             <div>Name: Star Wars55</div>
+                             <div>Price: $0.08</div></div>
                         
-                        <div>Quanity: 1</div>
                     
-                    </div>
-                    <div className="mainPage-container">
-                        <div className="image">
-                            <img src="http://lorempixel.com/702/200/" />
-                        </div>
-                    
-                        <div className="column">
-                            <div>Name: Star Wars55</div>
-                            <div>Price: $0.08</div></div>
-                        
-                        <div>Quanity: 1</div>
-                    
-                    </div>
-            
-            
-            
-            
-            </div>
+                     </div>
 
 
+
+        </Slider>
+      </div>
 
             
         )
@@ -64,3 +59,51 @@ class MainPageContainer extends React.Component{
 }
 
 export default MainPageContainer
+
+
+
+// <div classNameName="parent">
+
+            //         <div classNameName="mainPage-container">
+            //             <div classNameName="image">
+            //                 <img src="http://lorempixel.com/702/200/" />
+            //             </div>
+                    
+            //             <div classNameName="column">
+            //                 <div>Name: Star Wars55</div>
+            //                 <div>Price: $0.08</div></div>
+                        
+            //             <div>Quanity: 1</div>
+                    
+            //         </div>
+                    
+            //         <div classNameName="mainPage-container">
+            //             <div classNameName="image">
+            //                 <img src="http://lorempixel.com/702/200/" />
+            //             </div>
+                    
+            //             <div classNameName="column">
+            //                 <div>Name: Star Wars55</div>
+            //                 <div>Price: $0.08</div></div>
+                        
+            //             <div>Quanity: 1</div>
+                    
+            //         </div>
+            //         <div classNameName="mainPage-container">
+            //             <div classNameName="image">
+            //                 <img src="http://lorempixel.com/702/200/" />
+            //             </div>
+                    
+            //             <div classNameName="column">
+            //                 <div>Name: Star Wars55</div>
+            //                 <div>Price: $0.08</div></div>
+                        
+            //             <div>Quanity: 1</div>
+                    
+            //         </div>
+            
+            
+            
+           
+
+            
